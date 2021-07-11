e0328e854c57734b9ec9d9c8f490d85360aa9791
mariamelesheva <mariamelesheva@ya.ru>
2021-07-11 20:03:33 +0700

    add order type enum, change dynamic sort to sort then insert,  change tests into parametrized

--- order_book.py
+++ order_book.py
@@ -1,70 +1,67 @@
+import enum
+
+
+class OrderType(enum.Enum):
+    ASK = 0
+    BID = 1
+
+
 class OrderBook:
     class Order:
 
-        def __init__(self, price: float, quantity: int):
+        def __init__(self, price: float, quantity: int, order_type: OrderType):
             self.price = price
             self.quantity = quantity
+            self.order_type = order_type
 
         def get_data_dictionary(self):
             return {"price": self.price, "quantity": self.quantity}
 
-    class Ask(Order):
-        pass
-
-    class Bid(Order):
-        pass
-
-    asks: list[Ask]
-    bids: list[Bid]
-
     def __init__(self):
-
         self.asks = []
         self.bids = []
 
-    def get_sorted_market_data(self):
-        return {"asks": list(map(lambda x: x.get_data_dictionary(), sorted(self.asks, key=lambda order: order.price))),
-                "bids": list(map(lambda x: x.get_data_dictionary(), sorted(self.bids, key=lambda order: order.price)))}
+    def get_market_data(self):
+        return {"asks": list(map(lambda x: x.get_data_dictionary(), self.asks)),
+                "bids": list(map(lambda x: x.get_data_dictionary(), self.bids))}
 
-    def remove(self, order):
-        if type(order) is OrderBook.Ask:
-            self.asks.remove(order)
-        elif type(order) is OrderBook.Bid:
-            self.bids.remove(order)
-        else:
-            print(f'wrong order type: {type(order)}')
+    def insert_order(self, orders_list: list, order: Order):
+        index = 0
+        orders_length = len(orders_list)
+        if orders_length == 0:
+            orders_list.append(order)
+            return
+        for i in range(orders_length):
+            if orders_list[i].price > order.price:
+                index = i
+                break
+            elif i == orders_length - 1:
+                orders_list.append(order)
+                return
+        orders_list.insert(index, order)
 
-    def create_bid(self, price: float, quantity: int):
-        err = self.__perform_checks(price, quantity)
-        if not err:
-            bid = OrderBook.Bid(price, quantity)
-            self.bids.append(bid)
-            print('bid added')
-            return bid
-        else:
-            print('bid was not added')
-
-    def create_ask(self, price: float, quantity: int):
-        err = self.__perform_checks(price, quantity)
-        if not err:
-            ask = OrderBook.Ask(price, quantity)
-            self.asks.append(ask)
-            print('ask added')
-            return ask
-        else:
-            print('ask was not added')
-
-    def __perform_checks(self, price, quantity):
+    def create_order(self, price: float, quantity: int, order_type: OrderType):
         if type(price) is not float and type(price) is not int:
             print(f'wrong price type: {type(price)}')
-            return True
+            return
         if price <= 0:
             print(f'price must be greater than 0')
-            return True
+            return
 
         if type(quantity) is not int:
             print(f'wrong quantity type: {type(quantity)}')
-            return True
+            return
         if quantity <= 0:
             print(f'quantity must be greater than 0')
-            return True
+            return
+
+        if type(order_type) is not OrderType:
+            print(f'wrong order type')
+            return
+
+        order = OrderBook.Order(price, quantity, order_type)
+        if order.order_type == OrderType.ASK:
+            self.insert_order(self.asks, order)
+        else:
+            self.insert_order(self.bids, order)
+        return order
--- test_order_book.py
+++ test_order_book.py
@@ -1,143 +1,80 @@
 import pytest
 
-from order_book import OrderBook
+from order_book import OrderBook, OrderType
 
 
-class TestAskPrice:
-    def test_create_ask_price_int_quantity_int(self):
-        order_book = OrderBook()
-        ask = order_book.create_ask(100, 200)
-        assert ask.price == 100
-        assert ask.quantity == 200
-        assert ask in order_book.asks
-        assert len(order_book.bids) == 0
+class TestCreateOrder:
 
-    def test_create_ask_price_float(self):
-        order_book = OrderBook()
-        ask = order_book.create_ask(60.90, 30)
-        assert ask.price == 60.90
-        assert ask.quantity == 30
-        assert ask in order_book.asks
-        assert len(order_book.bids) == 0
+    def setup(self):
+        self.order_book = OrderBook()
 
-    def test_create_ask_price_zero(self):
-        order_book = OrderBook()
-        ask = order_book.create_ask(0, 1200)
+    def create_order_and_check_created(self, price, quantity, order_type):
+        order = self.order_book.create_order(price, quantity, order_type)
+        assert order.price == price
+        assert order.quantity == quantity
+        assert order.order_type == order_type
+        return order
+
+    @pytest.mark.parametrize("order_type, price, quantity",
+                             [(OrderType.ASK, 100, 200),
+                              (OrderType.ASK, 60.90, 30)])
+    def test_create_ask_positive(self, order_type, price, quantity):
+        order = self.create_order_and_check_created(price, quantity, order_type)
+        assert order in self.order_book.asks
+        assert len(self.order_book.bids) == 0
+
+    @pytest.mark.parametrize("order_type, price, quantity",
+                             [(OrderType.BID, 100, 200),
+                              (OrderType.BID, 60.90, 30)])
+    def test_create_bid_positive(self, order_type, price, quantity):
+        order = self.create_order_and_check_created(price, quantity, order_type)
+        assert order in self.order_book.bids
+        assert len(self.order_book.asks) == 0
+
+    @pytest.mark.parametrize("order_type, price, quantity",
+                             [(OrderType.ASK, 0, 1200),
+                              (OrderType.ASK, -120, 1),
+                              (OrderType.ASK, '123', 666666),
+                              (OrderType.ASK, 30, 160.5),
+                              (OrderType.ASK, 100, 0),
+                              (OrderType.ASK, 5, -5),
+                              (OrderType.ASK, 66, [1]),
+
+                              (OrderType.BID, 0, 1200),
+                              (OrderType.BID, -120, 1),
+                              (OrderType.BID, '123', 666666),
+                              (OrderType.BID, 30, 160.5),
+                              (OrderType.BID, 100, 0),
+                              (OrderType.BID, 5, -5),
+                              (OrderType.BID, 66, [1])
+                              ])
+    def test_negative(self, order_type, price, quantity):
+        ask = self.order_book.create_order(price, quantity, order_type)
         assert ask is None
-        assert len(order_book.asks) == 0
-        assert len(order_book.bids) == 0
+        assert len(self.order_book.asks) == 0
+        assert len(self.order_book.bids) == 0
 
-    def test_create_ask_price_negative(self):
+
+class TestGetMarketData:
+    def test_get_market_data(self):
         order_book = OrderBook()
-        ask = order_book.create_ask(-120, 1)
-        assert ask is None
-        assert len(order_book.asks) == 0
-        assert len(order_book.bids) == 0
+        order_book.create_order(100.5, 10, OrderType.ASK)
+        order_book.create_order(300, 400, OrderType.BID)
+        order_book.create_order(300, 400, OrderType.BID)
+        order_book.create_order(60, 1, OrderType.ASK)
+        order_book.create_order(100.4, 4, OrderType.ASK)
+        order_book.create_order(100.5, 10, OrderType.ASK)
+        order_book.create_order(60.6, 4, OrderType.BID)
+        order_book.create_order(100.5, 10, OrderType.BID)
+        order_book.create_order(100.5, 9, OrderType.ASK)
 
-    def test_create_ask_price_wrong_type(self):
-        order_book = OrderBook()
-        ask = order_book.create_ask('123', 666666)
-        assert ask is None
-        assert len(order_book.asks) == 0
-        assert len(order_book.bids) == 0
-
-
-class TestAskQuantity:
-
-    def test_create_ask_quantity_float(self):
-        order_book = OrderBook()
-        ask = order_book.create_ask(30, 160.5)
-        assert ask is None
-        assert len(order_book.asks) == 0
-        assert len(order_book.bids) == 0
-
-    def test_create_ask_quantity_zero(self):
-        order_book = OrderBook()
-        ask = order_book.create_ask(100, 0)
-        assert ask is None
-        assert len(order_book.asks) == 0
-        assert len(order_book.bids) == 0
-
-    def test_create_ask_quantity_negative(self):
-        order_book = OrderBook()
-        ask = order_book.create_ask(5, -5)
-        assert ask is None
-        assert len(order_book.asks) == 0
-        assert len(order_book.bids) == 0
-
-    def test_create_ask_quantity_wrong_type(self):
-        order_book = OrderBook()
-        ask = order_book.create_ask(66, [1])
-        assert ask is None
-        assert len(order_book.asks) == 0
-        assert len(order_book.bids) == 0
-
-
-class TestBidPrice:
-    def test_create_bid_price_int_quantity_int(self):
-        order_book = OrderBook()
-        bid = order_book.create_bid(100, 200)
-        assert bid.price == 100
-        assert bid.quantity == 200
-        assert bid in order_book.bids
-        assert len(order_book.asks) == 0
-
-    def test_create_bid_price_float(self):
-        order_book = OrderBook()
-        bid = order_book.create_bid(60.90, 30)
-        assert bid.price == 60.90
-        assert bid.quantity == 30
-        assert bid in order_book.bids
-        assert len(order_book.asks) == 0
-
-    def test_create_bid_price_zero(self):
-        order_book = OrderBook()
-        bid = order_book.create_bid(0, 1200)
-        assert bid is None
-        assert len(order_book.bids) == 0
-        assert len(order_book.asks) == 0
-
-    def test_create_bid_price_negative(self):
-        order_book = OrderBook()
-        bid = order_book.create_bid(-120, 1)
-        assert bid is None
-        assert len(order_book.bids) == 0
-        assert len(order_book.asks) == 0
-
-    def test_create_bid_price_wrong_type(self):
-        order_book = OrderBook()
-        bid = order_book.create_bid('123', 666666)
-        assert bid is None
-        assert len(order_book.bids) == 0
-        assert len(order_book.asks) == 0
-
-
-class TestBidQuantity:
-
-    def test_create_bid_quantity_float(self):
-        order_book = OrderBook()
-        bid = order_book.create_bid(30, 160.5)
-        assert bid is None
-        assert len(order_book.bids) == 0
-        assert len(order_book.asks) == 0
-
-    def test_create_bid_quantity_zero(self):
-        order_book = OrderBook()
-        bid = order_book.create_bid(100, 0)
-        assert bid is None
-        assert len(order_book.bids) == 0
-        assert len(order_book.asks) == 0
-
-    def test_create_bid_quantity_negative(self):
-        order_book = OrderBook()
-        bid = order_book.create_bid(5, -5)
-        assert bid is None
-        assert len(order_book.bids) == 0
-        assert len(order_book.asks) == 0
-
-    def test_create_bid_quantity_wrong_type(self):
-        order_book = OrderBook()
-        bid = order_book.create_bid(66, [1])
-        assert bid is None
-        assert len(order_book.bids) == 0
-        assert len(order_book.asks) == 0
+        orders_dictionary = {"asks": [{"price": 60, "quantity": 1},
+                                      {"price": 100.4, "quantity": 4},
+                                      {"price": 100.5, "quantity": 10},
+                                      {"price": 100.5, "quantity": 10},
+                                      {"price": 100.5, "quantity": 9}],
+                             "bids": [{"price": 60.6, "quantity": 4},
+                                      {"price": 100.5, "quantity": 10},
+                                      {"price": 300, "quantity": 400},
+                                      {"price": 300, "quantity": 400}]}
+        assert order_book.get_market_data() == orders_dictionary
